7c0897985852230bd0fee708e5f6680bd1de2632
Phạm Quyền <quyenph@ichiba.vn>
2023-03-09 10:25:09 +0700

    initial commit

--- template.config.js
+++ template.config.js
@@ -1,4 +1,11 @@
 module.exports = {
-  placeholderName: "RNTemplate",
+  // Placeholder used to rename and replace in files
+  // package.json, index.json, android/, ios/
+  placeholderName: "template",
+
+  // Directory with template
   templateDir: "./template",
+
+  // Path to script, which will be executed after init
+  postInitScript: "./script.js",
 };
