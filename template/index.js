609c488b7ff7dc8339515098f842043eb184273d
Phạm Quyền <quyenph@ichiba.vn>
2023-03-09 14:39:13 +0700

    logBox & delete package-lock

--- template/index.js
+++ template/index.js
@@ -2,8 +2,19 @@
  * @format
  */
 
-import { AppRegistry } from "react-native";
+import { AppRegistry, LogBox } from "react-native";
 import App from "./src/App";
 import { name as appName } from "./app.json";
+
+const ignoreWarns = [
+  //   "Setting a timer for a long period of time",
+  //   "VirtualizedLists should never be nested inside plain ScrollViews with the same orientation",
+  "ViewPropTypes will be removed from React Native",
+  //   "AsyncStorage has been extracted from react-native",
+  //   "EventEmitter.removeListener",
+];
+
+LogBox.ignoreLogs(ignoreWarns);
+console.reportErrorsAsExceptions = false;
 
 AppRegistry.registerComponent(appName, () => App);
